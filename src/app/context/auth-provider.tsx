f4c94ada722085f15045de68bbcbcedaaa21d054
HtetAung99 <htetaung251299@gmail.com>
2024-02-15 23:53:58 +0000

    updated todo.txt

--- src/app/context/auth-provider.tsx
+++ src/app/context/auth-provider.tsx
@@ -8,3 +8,29 @@ export default function AuthProvider({
 }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
+// loc = {
+//   address: {
+//     address1: "TW20 0LG",
+//     address2: "Englefield Green, Egham",
+//     aptOrSuite: "",
+//     eaterFormattedAddress: "Englefield Green, Egham TW20 0LG, UK",
+//     subtitle: "Englefield Green, Egham",
+//     title: "TW20 0LG",
+//     uuid: "",
+//   },
+//   latitude: 51.436864,
+//   longitude: -0.5631267999999999,
+//   reference: "ChIJv9dj26lwdkgRXSUYxRc-O44",
+//   referenceType: "google_places",
+//   type: "google_places",
+//   addressComponents: {
+//     city: "Egham",
+//     countryCode: "GB",
+//     firstLevelSubdivisionCode: "England",
+//     postalCode: "TW20 0LG",
+//   },
+//   categories: ["postal_code"],
+//   originType: "user_autocomplete",
+//   source: "manual_auto_complete",
+//   userState: "Unknown",
+// };
